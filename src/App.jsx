1da7a336c90a9dc7d780fd3c35db89f0a1b7254d
Ved Yadav <vedyadav19057@gmail.com>
2026-01-27 17:07:15 +0530

    Merge remote-tracking branch 'origin/master'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 import { supabase } from './supabaseClient'
 import Login from './features/auth/Login'
 import Dashboard from './features/home/Dashboard'
